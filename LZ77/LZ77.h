9416b94eb4a08bc03f4fd494d34b01358580627d
Samuel Wolf <samuelwolf94@googlemail.com>
2018-04-02 17:33:28 +0200

    Added GPL

--- LZ77/LZ77.h
+++ LZ77/LZ77.h
@@ -1,3 +1,18 @@
+// Vuong-DCP Codec, copyright 2018 by Samuel Wolf
+//
+//This program is free software : you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program.If not, see <http://www.gnu.org/licenses/>.
+
 #include <Vfw.h>
 #define DllExport   __declspec( dllexport )  
 
@@ -14,6 +29,6 @@ extern "C" DllExport unsigned long  getCompressID() {
 
 // Compression Method Name 
 extern "C"  DllExport const char * getCompressName() {
-	return "LZ77(2048/1024)";
+	return "LZ77(200/100)";
 };
 
